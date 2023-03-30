71d4be4b42b92358c997abf1f0fef3793f603e79
hatum89 <juanhatumvarela@gmail.com>
2023-03-29 23:06:12 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {JwksValidationHandler, OAuthService} from 'angular-oauth2-oidc';
+import {authCodeFlowConfig} from './oAuth.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OAUTH2';
+  constructor( private oauthService: OAuthService) {
+    this.configureOauth();
+  }
+
+  configureOauth() {
+   this.oauthService.configure(authCodeFlowConfig);
+   this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+   // this.oauthService.loadDiscoveryDocumentAndTryLogin().then();
+   this.oauthService.loadDiscoveryDocumentAndLogin().then();
+  }
+  // login() {
+  //  this.oauthService.initImplicitFlow();
+  // }
+  //
+  // logout() {
+  //  this.oauthService.logOut();
+  // }
+  get token() {
+    let claims: any = this.oauthService.getIdentityClaims();
+    return claims ? claims  : null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {OAuthModule} from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
